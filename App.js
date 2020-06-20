395f37d234c1e39cc00d33b902d10afdb83ac520
Shashank Lal <shashanklal01@gmail.com>
2020-06-20 19:52:15 +0400

    Adds components

--- App.js
+++ App.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/addTodo';
+
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' },
+  ])
+
+  const pressHandler = (key) => {
+    setTodos(prev => {
+      return prev.filter(todo => todo.key !== key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    if (text.length > 0) {
+      setTodos(prev => {
+        return [
+          { text: text, key: Math.random().toString() },
+          ...prev
+        ]
+      })
+    } else {
+      Alert.alert('oops!', 'your todo doesn\'t have any text!', [
+        { text: 'understood', onPress: () => console.log('alert closed') }
+      ])
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    //<Sandbox />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => {
+                return (
+                  <TodoItem item={item} pressHandler={pressHandler} />
+                )
+              }}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -13,7 +60,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    flex: 1,
+    marginTop: 20,
+  }
 });
